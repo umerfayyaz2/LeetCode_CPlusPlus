74c1930740a98981bdcaa56dddce6495841e4eff
umerfayyaz2~ <ufayyaz19@gmail.com>
2024-09-05 00:44:21 +0500

    Changed Node Values

--- 2_Linked_List/get_node_value.cpp
+++ 2_Linked_List/get_node_value.cpp
@@ -37,7 +37,7 @@ int main()
     head->next = new ListNode(2);
     head->next->next = new ListNode(3);
     head->next->next->next = new ListNode(4);
-    head->next->next->next->next = new ListNode(5);
+    head->next->next->next->next = new ListNode(6);
 
     int k = 2;
     ListNode *result = getKthFromEnd(head, k);
