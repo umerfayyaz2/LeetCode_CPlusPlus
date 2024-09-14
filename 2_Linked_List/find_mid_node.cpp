ea402d7aa3dd30a731740266c4305b0c6626c01b
umerfayyaz2~ <ufayyaz19@gmail.com>
2024-09-14 15:01:04 +0500

    Added find_mid_node solution

--- 2_Linked_List/find_mid_node.cpp
+++ 2_Linked_List/find_mid_node.cpp
@@ -22,29 +22,6 @@ int find_middle_node(nodeType *head)
     return slow->value;
 }
 
-#include <iostream>
-
-struct nodeType
-{
-    int value;
-    nodeType *next;
-    nodeType(int x) : value(x), next(nullptr) {}
-};
-
-int ind_middle_node(nodeType *head)
-{
-    nodeType *slow = head;
-    nodeType *fast = head;
-
-    while (fast != nullptr && fast->next != nullptr)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-
-    return slow->value;
-}
-
 int main()
 {
     nodeType *head = new nodeType(1);
@@ -53,7 +30,7 @@ int main()
     head->next->next->next = new nodeType(4);
     head->next->next->next->next = new nodeType(5);
 
-    cout << "Middle node valueue: " << ind_middle_node(head) << endl;
+    cout << "Middle node valueue: " << find_middle_node(head) << endl;
 
     return 0;
 }
