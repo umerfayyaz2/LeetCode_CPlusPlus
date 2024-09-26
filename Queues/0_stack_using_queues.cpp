b5dfbc4aa697ce75f2d69e2276cb42b86c574d9f
umerfayyaz2~ <ufayyaz19@gmail.com>
2024-09-26 21:22:25 +0500

    Added Stack using queues problem

---
+++ Queues/0_stack_using_queues.cpp
@@ -0,0 +1,63 @@
+class MyStack
+{
+private:
+    queue<int> q1; // Primary queue to hold elements
+    queue<int> q2; // Secondary queue used for pop and top operations
+
+public:
+    // Constructor
+    MyStack() {}
+
+    // Push element x onto stack
+    void push(int x)
+    {
+        q1.push(x);
+    }
+
+    // Removes the element on the top of the stack and returns it
+    int pop()
+    {
+        // Move all elements except the last one to q2
+        while (q1.size() > 1)
+        {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        // Get the last element which is the top of the stack
+        int top_element = q1.front();
+        q1.pop(); // Remove the top element
+
+        // Swap q1 and q2
+        swap(q1, q2);
+
+        return top_element;
+    }
+
+    // Get the top element
+    int top()
+    {
+        // Similar to pop but don't remove the last element
+        while (q1.size() > 1)
+        {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        // Get the last element which is the top of the stack
+        int top_element = q1.front();
+
+        // Move the top element to q2 to maintain stack order
+        q2.push(top_element);
+        q1.pop();
+
+        // Swap q1 and q2
+        swap(q1, q2);
+
+        return top_element;
+    }
+
+    // Returns whether the stack is empty
+    bool empty()
+    {
+        return q1.empty();
+    }
+};
